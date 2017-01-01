036305955779508f479a2d4cb9f7d7af73904824
mousezero <rm@mousezero.com>
2016-12-31 21:42:49 -0800

    hello world

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-// Set up your application entry point here...
+import React from 'react';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+import {Router, browserHistory} from 'react-router';
+import routes from './routes';
+import configureStore from './store/configureStore';
+import {syncHistoryWithStore} from 'react-router-redux';
+
+const store = configureStore();
+const history = syncHistoryWithStore(browserHistory, store);
+
+render(
+  <h1>Hello World</h1>,
+  document.getElementById('app')
+)
